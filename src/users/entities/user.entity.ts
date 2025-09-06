bd57ca8ec5c8abbb19c855bd96bcc788087040bf
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-06 06:01:49 -0500

    feat: :sparkles: add Entity User for mapping table user on database

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,9 +1,17 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class User {
-    public id: number;
+    @PrimaryGeneratedColumn()
+    id: number
 
-    public name: string;
 
-    public age: number;
+    @Column({ length: 255 })
+    name: string;
 
-    public email: string;
+    @Column('int')
+    age: number;
+
+    @Column({ length: 255 })
+    email: string;
 }
