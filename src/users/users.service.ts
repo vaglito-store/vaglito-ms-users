0b20aaaefd54d476569bb6e394edacde8ae8c9b2
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-06 06:21:04 -0500

    chore: Add message log database connected

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,14 +1,18 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, Logger } from "@nestjs/common";
 import { User } from "./interfaces/user.interface";
 import { Repository } from "typeorm";
 import { CreateUserDto } from "./dto/create-user.dto";
 
 @Injectable()
 export class UserService {
+    private readonly logger =  new Logger('UsersService')
+
     constructor(
         @Inject('USER_REPOSITORY')
         private userRepository: Repository<User>
-    ) {}
+    ) {
+        this.logger.log('Database connected')
+    }
 
     async create(createUserDto: CreateUserDto): Promise<User> {
         const new_user = this.userRepository.create(createUserDto)
