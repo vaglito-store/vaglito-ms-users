2cd476fde6a222a657f97aa2e6e9ef0cf42b9504
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-07 03:38:14 -0500

    feat: :sparkles: add pagination in service findAll

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,25 +1,36 @@
-import { Inject, Injectable, Logger } from "@nestjs/common";
-import { User } from "./interfaces/user.interface";
-import { Repository } from "typeorm";
-import { CreateUserDto } from "./dto/create-user.dto";
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class UserService {
-    private readonly logger =  new Logger('UsersService')
+  private readonly logger = new Logger('UsersService');
 
-    constructor(
-        @Inject('USER_REPOSITORY')
-        private userRepository: Repository<User>
-    ) {
-        this.logger.log('Database connected')
-    }
+  constructor(
+    @Inject('USER_REPOSITORY')
+    private userRepository: Repository<User>,
+  ) {
+    this.logger.log('Database connected');
+  }
 
-    async create(createUserDto: CreateUserDto): Promise<User> {
-        const new_user = this.userRepository.create(createUserDto)
-        return this.userRepository.save(new_user)
-    }
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const new_user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(new_user);
+  }
 
-    async findAll(): Promise<User[]> {
-        return this.userRepository.find()
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+    const [data, total] = await this.userRepository.findAndCount({
+        skip: (page - 1) * limit,
+        take: limit
+    });
+    return {
+        total,
+        page,
+        lastPage: Math.ceil(total/limit),
+        data,
     }
+  }
 }
