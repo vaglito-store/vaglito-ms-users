a3194db5c7325863ed39b738c74d24ef9610c4e2
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-07 18:38:03 -0500

    feat: :sparkles: add id

--- src/users/dto/update.user.dto.ts
+++ src/users/dto/update.user.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsNumber, IsPositive } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsNumber()
+    @IsPositive()
+    id: number;
+}
