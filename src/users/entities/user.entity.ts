dc9b9c78828baee081c7780ead112783fda09710
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-07 05:21:05 -0500

    feat: :sparkles: add Column available and delete_at

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,17 +1,29 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  DeleteDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number;
 
+  @Column({
+    default: true,
+  })
+  available: boolean;
 
-    @Column({ length: 255 })
-    name: string;
+  @Column({ length: 255 })
+  name: string;
 
-    @Column('int')
-    age: number;
+  @Column('int')
+  age: number;
 
-    @Column({ length: 255 })
-    email: string;
+  @Column({ length: 255 })
+  email: string;
+
+  @DeleteDateColumn()
+  delete_at: string;
 }
