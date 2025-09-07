ce393a12fd4b16941ba94818445e30194eb34002
Cesar Gonzalez <cesargonzalez390@gmail.com>
2025-09-07 17:32:19 -0500

    feat: :sparkles: change settings rest to microservices transport tcp

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { environmentVars } from './config';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Main')
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule, 
+    {
+      transport: Transport.TCP,
+      options: {
+        port: environmentVars.port
+      }
+    },
+  );
+
+
+
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(environmentVars.port);
-  logger.log(`App running on port ${environmentVars.port}`)
+  await app.listen();
+  logger.log(`Users Microservice running on port ${environmentVars.port}`)
 }
 bootstrap();
